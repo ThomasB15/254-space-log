fdb33c0e1a6112e8be11d7d8716000d20d46354c
anthony17serrato <anthony17@csu.fullerton.edu>
2018-04-05 21:35:52 -0700

    addeed code

--- terraformable.py
+++ terraformable.py
@@ -1,1 +1,11 @@
 //get number of terraformable planets
+import re
+
+def get_total_fuel(content:str) -> float:
+	pattern = re.compile("\"FuelUsed\":(\d+\.\d+)")
+	result = pattern.findall(content)
+	fuel = 0
+	if result:
+		for r in result:
+			fuel+=float(r)
+	return fuel
