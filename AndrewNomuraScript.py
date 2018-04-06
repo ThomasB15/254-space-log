4a4910505fe57675d7f0bde8f5597ca80701d888
Andrew Nomura <drew97.nomura@gmail.com>
2018-04-06 12:18:00 -0700

    finished problem 1

--- AndrewNomuraScript.py
+++ AndrewNomuraScript.py
@@ -3,7 +3,7 @@
 import re
 
 def get_star_systems(content:str):
-	pattern = re.compile("\"StarSystem\"w+")
+	pattern = re.compile("\"StarSystem\":\"\w+")
 	result = pattern.findall(content)
 	stars = 0
 
