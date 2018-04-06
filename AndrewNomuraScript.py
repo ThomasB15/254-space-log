05f80a60c1dd37a4c3ba7f24e2c42a6781142732
Andrew Nomura <drew97.nomura@gmail.com>
2018-04-06 10:32:52 -0700

    added a function

--- AndrewNomuraScript.py
+++ AndrewNomuraScript.py
@@ -1,2 +1,11 @@
-print("test")
 
+import starsystems
+
+def get_start_systems("event":"FSDJump") -> string:
+	pattern = starsystems.compile("\"StarSystems visited\":(\d+\.\d+)")
+	result = pattern.findall(content)
+	stars = 0
+	if result:
+		for s in result:
+			starts+=string(s)
+return stars
