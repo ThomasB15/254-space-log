ce58d118681c699e2f8fea056b70e88d942480c1
ThomasB15 <tommyb@csu.fullerton.edu>
2018-04-05 22:36:55 -0700

    Program running, Prints all names

--- planets.py
+++ planets.py
@@ -1,7 +1,7 @@
 # THomas Brenner 
 import re
 def names_of_planets(content:str):
-		pattern = re.compile("\"BodyName\":\"\w+")
+		pattern = re.compile("\"BodyName\":\"[A-Za-z0-9 -]+")
 		result = pattern.findall(content)
 		
 		return result
