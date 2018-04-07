92a673e09af9fafd49554c96f52b44c6255f7192
anthony17serrato <anthony17@csu.fullerton.edu>
2018-04-06 19:16:03 -0700

    New code

--- terraformable.py
+++ terraformable.py
@@ -2,12 +2,12 @@
 import re
 
 def get_terraformable_planets(content:str) -> float:
-	pattern = re.compile("\"TerraformState\":(\S)")
+	pattern = re.compile("\"TerraformState\":\"\w+")
 	result = pattern.findall(content)
 	count = 0
 	if result:
 		for r in result:
 			if str(r):
-				print r
+				#print(str(r))
 				count+= 1
 	return count
