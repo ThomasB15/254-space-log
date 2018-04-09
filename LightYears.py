3525bd66383f75c211b134c8309b7127d44f3b07
root <root@debian.nachos576.com>
2018-04-08 17:07:38 -0700

    Updated LightYears

--- LightYears.py
+++ LightYears.py
@@ -1,1 +1,11 @@
 #Total Light Years
+import re
+
+def get_total_light_years(content:str) -> float:
+	pattern = re.compile("\"JumpDist\":(\d+\.\d+)")
+	result = pattern.findall(content)
+	dist = 0
+	if result:
+		for r in result:
+			dist+=float(r)
+return dist
